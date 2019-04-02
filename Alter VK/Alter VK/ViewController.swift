c71104b666197c6d7ab2f5e070494df4f5ffaa6b
Mahankov <anton.mahankov@yandex.ru>
2019-04-02 12:09:00 +0300

    Lesson 1. Added prototype of UI for VK Client

---
+++ Alter VK/Alter VK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alter VK
+//
+//  Created by Anton Makhankov on 02/04/2019.
+//  Copyright © 2019 Anton Makhankov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
